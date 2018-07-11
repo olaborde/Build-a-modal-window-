1faf5ee7144eb2ebe1188aada7c2a127070a80e0
olaborde <oml007@live.com>
2018-07-11 16:43:08 -0400

    click the button changes its color to red

--- js/index.js
+++ js/index.js
@@ -2,3 +2,10 @@
 //Select the button element
 const button = document.querySelector('.button');
 console.log(button);
+
+// add an event listener
+//callbacks
+//changinges classes
+button.addEventListener("click", function(){
+  button.classList.add('red')
+})
